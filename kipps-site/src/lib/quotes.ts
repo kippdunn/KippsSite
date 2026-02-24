ad87e1aa1c101854061ef221f53d3a64e69506dc
Kipp Dunn <kipp.dunn@infernored.com>
2026-02-24 17:00:45 -0500

    Rename files for convention

--- kipps-site/src/lib/quotes.ts
+++ kipps-site/src/lib/quotes.ts
@@ -4,7 +4,7 @@ import { remark } from "remark";
 import html from "remark-html";
 
 export const getQuotesData = async () => {
-  const quotesPath = path.join(process.cwd(), "quotes", "Quotes.md");
+  const quotesPath = path.join(process.cwd(), "quotes", "quotes.md");
   const fileContents = fs.readFileSync(quotesPath, "utf-8");
   const processedContent = await remark().use(html).process(fileContents);
   const contentHtml = processedContent.toString();
